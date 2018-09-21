10e74d240da4e8908b8a9221a437d754f90a9542
Lee Unsworth <lee.unsworth@loveholidays.com>
2018-09-21 15:43:12 +0100

    Adds delay for djano to wait for db to be ready

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+import time
 
 if __name__ == "__main__":
+    time.sleep(2)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
     try:
         from django.core.management import execute_from_command_line
